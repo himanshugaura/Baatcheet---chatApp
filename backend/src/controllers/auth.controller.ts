107f8b81b093d7701714c14097a32ed0505999ea
himanshu <himanshu32505@gmail.com>
2025-07-13 23:11:46 +0530

    cross origin cookie google login

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -111,7 +111,7 @@ export const googleLogin = asyncErrorHandler(
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
