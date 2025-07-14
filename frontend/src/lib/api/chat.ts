55b557822827a3d6496bda99b3e1589c7003b0fc
himanshu <himanshu32505@gmail.com>
2025-07-14 13:26:53 +0530

    send message bug fix

--- frontend/src/lib/api/chat.ts
+++ frontend/src/lib/api/chat.ts
@@ -21,32 +21,12 @@ export const fetchMessages = (userId: string, receiverId: string) => async (disp
   }
 };
 
-export const fetchOnlineStatus = (receiverId: string) => async (dispatch: AppDispatch): Promise<boolean> => {
-  try {
-    const res = await apiConnector("GET", ChatEndpoints.GET_ONLINE_STATUS_API( receiverId));
-    
-    if (res.success) {
-      if (res.data) {
-        const isOnline = true;
-        dispatch(setReceiverOnline(isOnline));
-      }
-      return true;
-    } else {
-      return false;
-    }
-  } catch (error) {
-    console.error("GetUserData error:", error);
-    return false;
-  }
-};
-
 export const sendMessage = (senderId: string , receiverId: string , text : string , roomId : string , replyTo : string | null) => async (dispatch: AppDispatch): Promise<boolean> => {
   try {
     const res = await apiConnector<Message>("POST", ChatEndpoints.SEND_MESSAGE_API , {senderId, receiverId, text, roomId, replyTo});
       
     if (res.success && res.data) {
       dispatch(fetchMessages(senderId , receiverId));
-      dispatch(addMessage(res.data))
       return true;
     } else {
       return false;
